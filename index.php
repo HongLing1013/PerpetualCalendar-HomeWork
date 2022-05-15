a1eed1fd28f5a245073a636aa138cdce201286b0
Hollie <tsubasababy1013@gmail.com>
2022-05-15 09:04:35 +0800

    替萬年曆製作了一個小的標題 使用毛玻璃 做成hover效果 覆蓋上下月控制區按鈕

--- index.php
+++ index.php
@@ -124,6 +124,7 @@
     }
 
     /* 動態背景 結束 */
+
     .box {
       position: relative;
       /* 因為body有偽元素 所以z-index要設定高一點 */
@@ -139,20 +140,46 @@
     /* 毛玻璃外框 開始 */
     .glass {
       position: relative;
-      /* 設定尺寸 */
+      padding: 0px;
+      margin: 0px;
+      font-size: 20px;
+      background: rgba(255, 255, 255, 0.1);
       width: 900px;
       height: 560px;
-      /* 設定版面顏色及框線 */
-      background: rgba(255, 255, 255, 0.1);
-      box-shadow: 0 25px 45px rgba(0, 0, 0, 0.1);
-      border: 1px solid rgba(255, 255, 255, 0.5);
-      border-right: 1px solid rgba(255, 255, 255, 0.2);
-      border-bottom: 1px solid rgba(255, 255, 255, 0.2);
-      border-radius: 10px;
-      /* 設置毛玻璃效果 */
-      backdrop-filter: blur(25px);
+      border-radius: 15px;
     }
     /* 毛玻璃外框 結束 */
+
+    /* 毛玻璃標題小毛玻璃 開始 */
+    .glass::before {
+      content: attr(data-title);
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: absolute;
+      top: 0.5rem;
+      left: 0.5rem;
+      width: 890px;
+      height: 50px;
+      background: rgba(255, 255, 255, 0.3);
+      font-size: 25px;
+      text-transform: uppercase;
+      font-weight: 900;
+      backdrop-filter: blur(25px);
+      border-radius: 15px;
+      transition: all 0.8s ease-in-out;
+      line-height: 15px;
+    }
+    /* 毛玻璃標題小毛玻璃 結束 */
+
+    /* 毛玻璃標題動態 開始 */
+    .glass:hover::before {
+      height: 4%;
+      top: 5px;
+      transform: translateY(-70%);
+      font-size: 23px;
+    }
+    /* 毛玻璃標題動態 結束 */
 
     /* 萬年曆排版 開始 */
     .wrapper {
@@ -165,11 +192,11 @@
       flex-wrap: wrap;
     }
 
-    /* 萬年曆排版 結束 */
-    section{
+    section {
       display: flex;
       justify-content: center;
     }
+
     .table {
       width: 651px;
       height: 651px;
@@ -184,7 +211,7 @@
       display: inline-block;
       width: 93px;
       height: 93px;
-      border: 1px solid rgba(255, 255, 255 , 0.2);
+      border: 1px solid rgba(255, 255, 255, 0.2);
       box-sizing: border-box;
       margin-left: -1px;
       margin-top: -1px;
@@ -197,11 +224,10 @@
       color: white;
       height: 32px;
       line-height: 30px;
-      font-family: 'Copperplate';
     }
 
     .weekend {
-      background: rgba(186, 85, 221 , 0.2);
+      background: rgba(186, 85, 221, 0.2);
     }
 
     .workday {
@@ -217,90 +243,91 @@
       justify-content: space-between;
       flex-basis: 100%;
     }
+        /* 萬年曆排版 結束 */
   </style>
 </head>
 
-<body>
-  <!-- 漸層動態背景 開始 -->
-  <div class="bg">
-    <div class="box">
-      <!-- 毛玻璃外框 開始 -->
-      <div class="glass">
-        <!-- 萬年曆樣式 開始 -->
-        <div class="wrapper">
-          <?php
-          // 取得上一個月跟下一個月份的參數
-          if (isset($_GET['month'])) {
-            $month = $_GET['month'];
-            $year = $_GET['year'];
-          } else {
-            $month = date('n');
-            $year = date("Y");
-          }
-          // 判斷1月以前跟12月以後的處理方式
-          switch ($month) {
-            case 1:
-              $prevMonth = 12;
-              $prevYear = $year - 1;
-              $nextMonth = $month + 1;
-              $nextYear = $year;
-              break;
-            case 12:
-              $prevMonth = $month - 1;
-              $prevYear = $year;
-              $nextMonth = 1;
-              $nextYear = $year + 1;
-              break;
-            default:
-              $prevMonth = $month - 1;
-              $prevYear = $year;
-              $nextMonth = $month + 1;
-              $nextYear = $year;
-          }
-          ?>
+<!-- 漸層動態背景 開始 -->
 
-          <!-- 控制切換月份的按鈕 開始-->
-          <div class="nav">
-            <span>
-              <a href="index.php?year=<?= $prevYear; ?>&month=<?= $prevMonth; ?>">上一個月</a>
-            </span>
-            <span><?= $year . '年' . $month . '月'; ?></span>
-            <span>
-              <a href="index.php?year=<?= $nextYear; ?>&month=<?= $nextMonth; ?>">下一個月</a>
-            </span>
-          </div>
-          <!-- 控制切換月份的按鈕 結束-->
+<body class="bg">
+  <div class="box">
+    <!-- 毛玻璃外框 開始 -->
+    <div class="glass" data-title="Calendar">
+      <!-- 萬年曆樣式 開始 -->
+      <div class="wrapper">
+        <?php
+        // 取得上一個月跟下一個月份的參數
+        if (isset($_GET['month'])) {
+          $month = $_GET['month'];
+          $year = $_GET['year'];
+        } else {
+          $month = date('n');
+          $year = date("Y");
+        }
+        // 判斷1月以前跟12月以後的處理方式
+        switch ($month) {
+          case 1:
+            $prevMonth = 12;
+            $prevYear = $year - 1;
+            $nextMonth = $month + 1;
+            $nextYear = $year;
+            break;
+          case 12:
+            $prevMonth = $month - 1;
+            $prevYear = $year;
+            $nextMonth = 1;
+            $nextYear = $year + 1;
+            break;
+          default:
+            $prevMonth = $month - 1;
+            $prevYear = $year;
+            $nextMonth = $month + 1;
+            $nextYear = $year;
+        }
+        ?>
 
-          <!-- 萬年曆內容 -->
-          <?php
-          // 設定各項參數
-          $firstDay = $year . "-" . $month . "-1"; //第一天的位子
-          $firstWeekday = date("w", strtotime($firstDay)); //第一天是星期幾
-          $monthDays = date("t", strtotime($firstDay));
-          $lastDay = $year . "-" . $month . "-" . $monthDays; //最後一天
-          $today = date("Y-m-d"); //取值今天
-          $lastWeekday = date("w", strtotime($lastDay)); //最後一天是星期幾
-          $dateHouse = [];
+        <!-- 控制切換月份的按鈕 開始-->
+        <div class="nav">
+          <span>
+            <a href="index.php?year=<?= $prevYear; ?>&month=<?= $prevMonth; ?>">上一個月</a>
+          </span>
+          <span><?= $year . '年' . $month . '月'; ?></span>
+          <span>
+            <a href="index.php?year=<?= $nextYear; ?>&month=<?= $nextMonth; ?>">下一個月</a>
+          </span>
+        </div>
+        <!-- 控制切換月份的按鈕 結束-->
 
-          for ($i = 0; $i < $firstWeekday; $i++) {
-            $dateHouse[] = "";
-          }
+        <!-- 萬年曆內容 -->
+        <?php
+        // 設定各項參數
+        $firstDay = $year . "-" . $month . "-1"; //第一天的位子
+        $firstWeekday = date("w", strtotime($firstDay)); //第一天是星期幾
+        $monthDays = date("t", strtotime($firstDay));
+        $lastDay = $year . "-" . $month . "-" . $monthDays; //最後一天
+        $today = date("Y-m-d"); //取值今天
+        $lastWeekday = date("w", strtotime($lastDay)); //最後一天是星期幾
+        $dateHouse = [];
 
-          for ($i = 0; $i < $monthDays; $i++) {
-            $date = date("Y-m-d", strtotime("+$i days", strtotime($firstDay)));
-            $dateHouse[] = $date;
-          }
+        for ($i = 0; $i < $firstWeekday; $i++) {
+          $dateHouse[] = "";
+        }
 
-          for ($i = 0; $i < (6 - $lastWeekday); $i++) {
-            $dateHouse[] = "";
-          }
+        for ($i = 0; $i < $monthDays; $i++) {
+          $date = date("Y-m-d", strtotime("+$i days", strtotime($firstDay)));
+          $dateHouse[] = $date;
+        }
 
-          ?>
-<!-- 萬年曆中間面板 開始 -->
-<section>
-            <!-- 左邊隨機圖 開始 -->
-          <div class="photo">
-          <img src="./img/month<?= $month; ?>-<?= rand(1, 2) ?>.jpg" width="210" height="490">
+        for ($i = 0; $i < (6 - $lastWeekday); $i++) {
+          $dateHouse[] = "";
+        }
+
+        ?>
+        <!-- 萬年曆中間面板 開始 -->
+        <section>
+          <!-- 左邊隨機圖 開始 -->
+          <div class="img">
+            <img src="./img/month<?= $month; ?>-<?= rand(1, 2) ?>.jpg" width="210" height="490">
           </div>
           <!-- 左邊隨機圖 結束 -->
           <!-- 印出萬年曆標題 開始-->
@@ -327,16 +354,15 @@
             ?>
           </div>
           <!-- 萬年曆結束 -->
-          </section>
-          <!-- 萬年曆中間面板 開始 -->
-        </div>
-        <!-- 萬年曆樣式 結束 -->
+        </section>
+        <!-- 萬年曆中間面板 開始 -->
       </div>
-      <!-- 毛玻璃外框 結束 -->
+      <!-- 萬年曆樣式 結束 -->
     </div>
-    <!-- 動態背景 結束 -->
+    <!-- 毛玻璃外框 結束 -->
   </div>
-  <!-- 漸層背景 結束 -->
+  <!-- 動態背景 結束 -->
 </body>
+<!-- 漸層背景 結束 -->
 
 </html>
